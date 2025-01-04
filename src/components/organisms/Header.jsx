712e123d947f9a124cbea313753089c430035045
Vincent <msvincentius@gmail.com>
2025-01-04 09:06:18 +0700

    Header image and web icon

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, ease: "easeOut" }}
       >
-        Erudite AI{" "}
-        <span className="text-neutral-400 text-xs md:text-sm ml-2">Beta</span>
+        <img src="src\assets\Erudite.svg" alt="Erudite AI" className="w-66 md:h-28 object-cover" />
+        <span className="text-neutral-400 text-xs md:text-sm mt-6">Beta</span> 
       </motion.h1>
       <motion.h2
         className="text-2xl md:text-3xl font-bold flex justify-center text-center my-16 md:my-32 px-4"
@@ -18,7 +18,7 @@ const Header = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, delay: 0.5, ease: "easeOut" }}
       >
-        Halo! Erudite Disini. Ingin Tanya Apa?
+        Halo! Erudite disini. Ingin Tanya Apa?
       </motion.h2>
     </header>
   );
