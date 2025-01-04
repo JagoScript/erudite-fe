b2d57e1d811be93985d951b0fab626c4ab476f54
Vincent <msvincentius@gmail.com>
2025-01-04 09:58:19 +0700

    Update bug

--- src/components/organisms/ChatBox.jsx
+++ src/components/organisms/ChatBox.jsx
@@ -1,55 +1,86 @@
 import { motion } from "framer-motion";
 import PropTypes from "prop-types";
+import { ToastContainer, toast, Slide } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { FaRegCopy } from "react-icons/fa";
 
-const ChatBox = ({ messages, isLoading, aiProfilePic }) => (
-  <div className="max-w-3xl mx-auto px-4 py-6 md:px-6">
-    {messages.map((message, index) => (
-      <motion.div
-        key={index}
-        className={`message mb-6 flex items-start ${
-          message.sender === "You" ? "justify-end" : "justify-start"
-        }`}
-        initial={{ opacity: 0, x: message.sender === "You" ? 50 : -50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        {message.sender !== "You" && (
-          <img
-            src={aiProfilePic}
-            alt="AI Profile"
-            className="w-10 h-10 mt-[12px] rounded-full mr-2 p-1 border-[1px] border-[#878685]"
-          />
-        )}
-        <div
-          className={`bubble p-2 rounded-lg max-w-[80%] md:max-w-[70%] ${
-            message.sender === "You"
-              ? "bg-blue-500 text-white self-end"
-              : "bg-neutral-800 text-white self-start"
-          } break-words`} // Add this class to enable text wrapping
+const ChatBox = ({ messages, isLoading, aiProfilePic }) => {
+  const handleCopy = (text) => {
+    navigator.clipboard.writeText(text);
+    toast.success('Pesan disalin ke clipboard.', {
+      position: "bottom-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Slide,
+      });
+  };
+
+  return (
+    <div className="max-w-3xl mx-auto px-4 py-6 md:px-6">
+      {messages.map((message, index) => (
+        <motion.div
+          key={index}
+          className={`message mb-6 flex items-start ${
+            message.sender === "You" ? "justify-end" : "justify-start"
+          }`}
+          initial={{ opacity: 0, x: message.sender === "You" ? 50 : -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.3 }}
         >
-          {message.text}
-        </div>
-      </motion.div>
-    ))}
-
-    {isLoading && (
-      <motion.div
-        className="message mb-2 flex items-start justify-start"
-        initial={{ opacity: 0, x: -50 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 0.3 }}
-      >
-        <div className="bubble p-2 rounded-lg bg-neutral-700 text-white self-start">
-          <div className="flex space-x-1">
-            <div className="w-2 h-2 bg-[#878685] rounded-full animate-bounce"></div>
-            <div className="w-2 h-2 bg-[#878685] rounded-full animate-bounce delay-200"></div>
-            <div className="w-2 h-2 bg-[#878685] rounded-full animate-bounce delay-400"></div>
+          {message.sender !== "You" && (
+            <img
+              src={aiProfilePic}
+              alt="AI Profile"
+              className="w-10 h-10 mt-[12px] rounded-full mr-2 p-1 border-[1px] border-[#878685]"
+            />
+          )}
+          <div>
+            <div
+              className={`bubble p-2 rounded-lg max-w-[95%] md:max-w-[90%] ${
+                message.sender === "You"
+                  ? "bg-blue-500 text-white self-end"
+                  : "bg-neutral-800 text-white self-start"
+              } break-words`} // Add this class to enable text wrapping
+            >
+              {message.text}
+            </div>
+            {message.sender !== "You" && (
+              <button
+                onClick={() => handleCopy(message.text)}
+                className="mt-2 ml-2 text-sm text-[#878685] hover:text-neutral-400 flex items-center"
+              >
+                <FaRegCopy className="mr-1" />
+              </button>
+            )}
           </div>
-        </div>
-      </motion.div>
-    )}
-  </div>
-);
+        </motion.div>
+      ))}
+
+      {isLoading && (
+        <motion.div
+          className="message mb-2 flex items-start justify-start"
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <div className="bubble p-2 rounded-lg bg-neutral-700 text-white self-start">
+            <div className="flex space-x-1">
+              <div className="w-2 h-2 bg-[#878685] rounded-full animate-bounce"></div>
+              <div className="w-2 h-2 bg-[#878685] rounded-full animate-bounce delay-200"></div>
+              <div className="w-2 h-2 bg-[#878685] rounded-full animate-bounce delay-400"></div>
+            </div>
+          </div>
+        </motion.div>
+      )}
+      <ToastContainer />
+    </div>
+  );
+};
 
 ChatBox.propTypes = {
   chatBoxRef: PropTypes.shape({
